ef3c24737444168cbfd861f701ba87e77a9a8a08
t.suzuki <dat-navel.617@kpa.biglobe.ne.jp>
2021-06-05 19:27:38 +0900

    初回リリース

---
+++ 01_実装/Google+の投稿をツイートする機能/GgtsInfo.php
@@ -0,0 +1,127 @@
+﻿<?php
+require_once("ErrorCheck.php");
+require_once("MemberCreater.php");
+
+class GgtsInfo
+{
+    /**
+     * Google+の投稿をツイートする機能
+     */
+     
+	public $common;					// 共通処理のオブジェクト
+	public $memberInfo;				// メンバー情報
+
+	// 最新記事
+	public $ggtsFile;				// Google+情報保存用ファイル
+	public $comentCounterFile;		// カウンタ保持ファイル
+	public $hokamenFile;			// 他メンバー情報保存用ファイル
+	
+	// 過去記事
+	public $ggtsFileOld;			// Google+情報保存用ファイル
+	public $comentCounterFileOld;	// カウンタ保持ファイル
+	public $hokamenFileOld;			// 他メンバー情報保存用ファイル
+
+	public $saishinFlg;				// 最新記事OR過去記事フラグ
+
+
+    /**
+     * コンストラクタ
+     *
+     * @param common 共通処理オブジェクト
+     * @param flg 最新記事：0,過去記事：1
+     */
+	function __construct($common, $flg) {
+		$this->common     = $common;
+		$this->saishinFlg = $flg;
+
+		// 最新記事
+		IF($this->saishinFlg) {
+			$this->ggtsFile    = $this->common->getTxtDir()."can.txt";
+			$this->comentCounterFile = $this->common->getTxtDir()."coment_counter.txt";
+			$this->hokamenFile = $this->common->getTxtDir()."hokamen.txt";
+			
+			$this->ggtsFileOld    = $this->common->getTxtDir()."old_can.txt";
+			$this->comentCounterFileOld = $this->common->getTxtDir()."old_coment_counter.txt";
+			$this->hokamenFileOld = $this->common->getTxtDir()."old_hokamen.txt";
+		}
+		// 過去記事
+		ELSE {
+			$this->ggtsFile    = $this->common->getTxtDir()."old_can.txt";
+			$this->comentCounterFile = $this->common->getTxtDir()."old_coment_counter.txt";
+			$this->hokamenFile = $this->common->getTxtDir()."old_hokamen.txt";
+		}
+	}
+
+    /**
+     * Google+情報を取得
+     * 更新無ければ空文字列を返却する。
+     *
+     * @return ツイートするメッセージ
+     */
+	public function getGgts() {
+		$message = "";
+
+		$contents = @file_get_contents($this->ggtsFile);
+		// 最新の記事URLを取得する
+		$memID = $this->common->getMemId();
+		$newKijiURL = $this->common->getNewUrlGgts($memID);
+
+		IF (strcmp($contents, $newKijiURL) == 0) {
+			$this->common->debug("not change.");
+			return "";
+		}
+		// 変更あった場合
+		ELSE {
+			// チャタリング除去
+			if(strcmp($this->common->getNewUrl_ggts($memID), $newKijiURL) == 0) {
+				// メンテナンス中の場合、値取得失敗する
+				if ($newKijiURL == "" || $newKijiURL == " ") {
+					echo "mentenance.\n";
+					return "";
+				}
+
+				// Google+記事取得
+				$hikakuKiji = $this->common->getNewKiji_ggts($newKijiURL);
+
+				// ツイッター連動チェック
+				IF($this->common->checkTwitterRendo($hikakuKiji)) {
+					if (mb_strlen($hikakuKiji, "UTF-8") > 55) {
+						$hikakuKiji = mb_substr($hikakuKiji,0,55,"UTF-8")."...";
+					}
+					$message = "(ツイ連)【Google+】 ".$hikakuKiji." ".$newKijiURL;
+				}
+				ELSE {
+					if (mb_strlen($hikakuKiji, "UTF-8") > 55) {
+						$hikakuKiji = mb_substr($hikakuKiji,0,55,"UTF-8")."...";
+					}
+					// 出力メッセージ
+					$message = "【Google+限定投稿】 ".$hikakuKiji." ".$newKijiURL;
+				}
+
+				// 処理実行した記事をOLDファイルに出力する。
+				$newGgtsFile = @file_get_contents($this->ggtsFile);
+				$newComentCounterFile = @file_get_contents($this->comentCounterFile);
+				$newHokamenFile = @file_get_contents($this->hokamenFile);
+				file_put_contents($this->ggtsFileOld, $newGgtsFile);
+				file_put_contents($this->comentCounterFileOld, $newComentCounterFile);
+				file_put_contents($this->hokamenFileOld, $newHokamenFile);
+				
+				file_put_contents($this->ggtsFile, $newKijiURL);
+				file_put_contents($this->comentCounterFile, "0");
+				file_put_contents($this->hokamenFile, "header");
+			}
+		}
+
+		return $message;
+    }
+
+    /**
+     * メンバ情報配列を取得する
+     *
+     * @return メンバ情報配列
+     */
+    private function getMemberArr() {
+		return $this->memberInfo->getMemNameArr();
+    }
+}
+?>
---
+++ 01_実装/ブログ更新をツイートする機能/BlogInfo.php
@@ -0,0 +1,93 @@
+﻿<?php
+class BlogInfo
+{
+	/**
+	 * ブログ更新をツイートする機能
+	 */
+
+	public $common;				// 共通処理オブジェクト
+	public $blogFile;			// ブログファイル名(blog.txt)
+
+	/**
+	 * コンストラクタ
+	 *
+	 * @param common 共通処理オブジェクト
+	 */
+	function __construct($common) {
+		$this->common = $common;
+		$this->blogFile = $this->common->getTxtDir()."blog.txt";
+		$this->common->debug($this->blogFile);
+	}
+
+	/**
+	 * ブログ情報を取得
+	 * 更新無ければ空文字列を返却する。
+	 *
+	 * @return ツイートするメッセージ
+	 */
+	public function getBlog() {
+		$message = "";
+
+		// ブログURL取得
+		$contents = @file_get_contents($this->blogFile);
+		// URLからテキスト取得
+		$urltxt = $this->common->getBlogURL();
+		$this->common->debug("urltxt: ".$urltxt);
+
+		// 最新のURL取得
+		$newUrl = $this->common->getNewUrlAmeba($urltxt);
+
+		// メンテナンス中の場合、値取得失敗する
+		if ($newUrl == "" || $newUrl == " ") {
+			echo "mentenance.\n";
+			return "";
+		}
+		
+		$this->common->debug($newUrl);
+		$this->common->debug("contents:".$contents);
+
+		// 等しい場合 = 0
+		if (strcmp($contents, $newUrl) == 0) {
+			$this->common->debug("not Change.");
+			return "";
+		}
+		// 変更がある場合
+		else{
+			// チャタリング除去
+			if(strcmp($this->common->getNewUrl_ameba($urltxt), $newUrl) == 0) {
+				// 変更有のため、処理
+				$blogTitle = $this->common->getNewTitle_ameba($urltxt);
+				// メンテナンス中の場合、値取得失敗する
+				if ($blogTitle == "" || $blogTitle == " ") {
+					echo "mentenance.\n";
+					return "";
+				}
+				
+				$this->common->debug($blogTitle);
+				
+				if (strlen($blogTitle) > 60) {
+					$blogTitle = substr($blogTitle,0,60);
+				}
+
+				// ファイル出力
+				file_put_contents($this->blogFile, $newUrl);
+
+				// アスキー文字を整形する
+				$blogTitle = $this->common->changeAscii($blogTitle);
+
+				// ※URL以外：46文字 URL：60文字（おおよそ実際は57か8）※
+				IF(mb_strlen($blogTitle, "UTF-8") < 30) {
+					// 通常版
+					$message = "【石田晴香ブログ】 はるきゃんのブログが更新されました。『 ".$blogTitle." 』 ".$newUrl."　#石田晴香 #はるきゃん";
+				}
+				ELSE {
+					// 簡素版
+					$message = "【石田晴香ブログ】 はるきゃんのブログが更新されました。（タイトル略） ".$newUrl;
+				}
+			}
+		}
+
+		return $message;
+    }
+}
+?>
